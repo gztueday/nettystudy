372d82124411577147f5dffddafbe1a309d2c039
wanggang <578620382@qq.com>
2020-07-09 02:28:46 +0800

    聊天室

---
+++ src/main/java/com/mashibing/nettystudy/s02/ClientFrame.java
@@ -0,0 +1,47 @@
+package com.mashibing.nettystudy.s02;
+
+import java.awt.BorderLayout;
+import java.awt.Frame;
+import java.awt.TextArea;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class ClientFrame extends Frame {
+	
+	TextArea ta = new TextArea();
+	TextField tf = new TextField();
+	
+	public ClientFrame() {
+		this.setSize(600, 400);
+		this.setLocation(100, 20);
+		this.add(ta, BorderLayout.CENTER);
+		this.add(tf, BorderLayout.SOUTH);
+		
+		tf.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//把字符串发送到服务器
+				ta.setText(ta.getText() + "\next" + tf.getText());
+				tf.setText("");
+			}
+		});
+		
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		this.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new ClientFrame();
+		
+	}
+
+}
