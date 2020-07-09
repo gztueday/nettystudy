18823a65db469ede09f220c6ca12b9874f783fe2
wanggang <578620382@qq.com>
2020-07-09 20:21:49 +0800

    EmmbeddedChannel实现Codec的单元测试

---
+++ src/test/java/nettyStudy/TankMsgCodecTest.java
@@ -0,0 +1,45 @@
+package nettyStudy;
+
+import org.junit.Test;
+
+import com.mashibing.nettystudy.s13.TankMsg;
+import com.mashibing.nettystudy.s13.TankMsgDecoder;
+import com.mashibing.nettystudy.s13.TankMsgEncoder;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.embedded.EmbeddedChannel;
+import junit.framework.Assert;
+
+public class TankMsgCodecTest {
+	
+	@Test
+	public void testTankMsgEncoder() {
+		TankMsg msg = new TankMsg(10, 10);
+		EmbeddedChannel ch = new EmbeddedChannel(new TankMsgEncoder());
+		ch.writeOutbound(msg);
+		
+		ByteBuf buf = (ByteBuf) ch.readOutbound();
+		int x = buf.readInt();
+		int y = buf.readInt();
+		
+		Assert.assertTrue(x == 10 && y == 10);
+		buf.release();
+	}
+
+	@Test
+	public void testTankMsgEncoder2() {
+		ByteBuf buf =  Unpooled.buffer();
+		TankMsg msg = new TankMsg(10, 10);
+		buf.writeInt(msg.x);
+		buf.writeInt(msg.y);
+		
+		EmbeddedChannel ch = new EmbeddedChannel(new TankMsgEncoder(), new TankMsgDecoder());
+		ch.writeInbound(buf.duplicate()); // 数据流的写入方向 <----------
+		
+		TankMsg tm = (TankMsg) ch.readInbound();
+		
+		Assert.assertTrue(tm.x == 10 && tm.y == 10);
+	}
+	
+}
---
+++ src/test/java/nettyStudy/TestCasetest.java
@@ -0,0 +1,14 @@
+package nettyStudy;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCasetest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
